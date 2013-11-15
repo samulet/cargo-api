2f79f885b95980fee448b89f62ae2a5a81d9f12b
salerat <salerat@gmail.com>
2013-11-15 23:25:42 +0700

    временно поправил юзера

--- module/User/src/User/Module.php
+++ module/User/src/User/Module.php
@@ -5,20 +5,7 @@ use ZF\Apigility\ApigilityModuleInterface;
 
 class Module implements ApigilityModuleInterface
 {
-    public function onBootstrap(MvcEvent $e)
-    {
-        $eventManager = $e->getApplication()->getEventManager();
 
-        $zfcServiceEvents = $e->getApplication()->getServiceManager()->get('zfcuser_user_service')->getEventManager();
-        $zfcServiceEvents->attach(
-            'register',
-            function ($e) {
-                /* @var $user \User\Entity\User */
-                $user = $e->getParam('user');
-                $user->addRole('user');
-            }
-        );
-    }
     public function getConfig()
     {
         return include __DIR__ . '/../../config/module.config.php';
