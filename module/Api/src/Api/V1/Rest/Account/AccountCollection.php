9730b223b310f5c49b1c278d0245b7e4e1441aa8
salerat <salerat@gmail.com>
2013-11-18 18:39:45 +0700

    вынесены в отдельное апи все модули реста

---
+++ module/Api/src/Api/V1/Rest/Account/AccountCollection.php
@@ -0,0 +1,8 @@
+<?php
+namespace Api\V1\Rest\Account;
+
+use Zend\Paginator\Paginator;
+
+class AccountCollection extends Paginator
+{
+}
---
+++ module/Api/src/Api/V1/Rest/Company/CompanyCollection.php
@@ -0,0 +1,8 @@
+<?php
+namespace Api\V1\Rest\Company;
+
+use Zend\Paginator\Paginator;
+
+class CompanyCollection extends Paginator
+{
+}
