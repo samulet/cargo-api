43b5e0bb109b4a016507092c494695b2065e5ce0
salerat <salerat@gmail.com>
2013-11-19 17:46:36 +0700

    правки в модуле аккаунт

--- module/Account/src/Account/Model/AccountModel.php
+++ module/Account/src/Account/Model/AccountModel.php
@@ -10,14 +10,9 @@ namespace Account\Model;
 
 use Account\Entity\Account;
 
-use Zend\ServiceManager\ServiceLocatorAwareInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
-use Doctrine\MongoDB\Connection;
-use Doctrine\ODM\MongoDB\Configuration;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 use Doctrine\ODM\MongoDB\Id\UuidGenerator;
-
 
 class AccountModel
 {
