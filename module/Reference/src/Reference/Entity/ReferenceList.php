28f4e91d1c7cb8a15585d1bb4df67658827052d6
salerat <salerat@gmail.com>
2013-12-06 23:20:33 +0700

    переделал референист лист, убрал статик

--- module/Reference/src/Reference/Entity/ReferenceList.php
+++ module/Reference/src/Reference/Entity/ReferenceList.php
@@ -9,11 +9,19 @@
 
 namespace Reference\Entity;
 
-class ReferenceListStatic {
-    public static $referenceList = array(
+class ReferenceList {
+    public $referenceList = array(
         'product-group' => array(
             'nameRus' => 'Продуктовая группа',
             'module' => 'ProductGroup'
         )
     );
+
+    public function getList($listName) {
+        if( !empty($referenceList[$listName]) ) {
+            return $referenceList[$listName];
+        } else {
+            return array();
+        }
+    }
 }
