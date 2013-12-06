9029bb4895668b447d1d02c730a780c54853c737
salerat <salerat@gmail.com>
2013-12-06 23:15:58 +0700

    первая версия статичиского листа списка

---
+++ module/Reference/src/Reference/Entity/ReferenceListStatic.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: salerat
+ * Date: 12/6/13
+ * Time: 10:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Reference\Entity;
+
+class ReferenceListStatic {
+    public static $referenceList = array(
+        'product-group' => array(
+            'nameRus' => 'Продуктовая группа',
+            'module' => 'ProductGroup'
+        )
+    );
+}
