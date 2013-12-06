ca08ae922229fcb43acda26ab9832b5b00f43d37
salerat <salerat@gmail.com>
2013-12-06 23:11:23 +0700

    сделал энтити для продуктовой группы

---
+++ module/Reference/src/Reference/AddList/ProductGroup/Entity/ProductGroup.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Account\AddList\ProductGroup\Entity;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+/**
+ * @ODM\Document(collection="product_group")
+ * @Gedmo\SoftDeleteable(fieldName="deletedAt")
+ */
+class ProductGroup
+{
+    /**
+     * @var string
+     * @ODM\Field(type="string")
+     */
+    protected $code;
+    /**
+     * @var string
+     * @ODM\Field(type="string")
+     */
+    protected $title;
+    /**
+     * @Gedmo\Timestampable(on="create")
+     * @ODM\Date
+     * @Annotation\Exclude()
+     */
+    protected $created_at;
+
+    /**
+     * @Gedmo\Timestampable(on="update")
+     * @ODM\Date
+     * @Annotation\Exclude()
+     */
+    protected $updated_at;
+    /**
+     * @ODM\Date
+     */
+    protected $deletedAt;
+
+    public function setData($data) {
+        if($data !== null && is_array($data)){
+            foreach(array_keys(get_class_vars(__CLASS__)) as $key){
+                if(isset($data[$key]) && ($key!='id') && ($key!='uuid') ){
+                    if(!is_array($this->$key)) {
+                        $this->$key = $data[$key];
+                    } else {
+                        $this->$key=$this->$key+$data[$key];
+                    }
+
+                }
+            }
+        }
+        return $this;
+
+    }
+
+    public function getData() {
+        $data = array();
+        foreach(array_keys(get_class_vars(__CLASS__)) as $key){
+            $data[$key]=$this->$key;
+        }
+        return $data;
+    }
+    /**
+     * @return mixed
+     */
+    public function getDeletedAt()
+    {
+        return $this->deletedAt;
+    }
+
+    /**
+     * @param mixed $deletedAt
+     */
+    public function setDeletedAt($deletedAt)
+    {
+        $this->deletedAt = $deletedAt;
+    }
+
+
+    public function getCreated()
+    {
+        return $this->created_at;
+    }
+
+    public function setCreated($created)
+    {
+        $this->created_at = $created;
+    }
+
+    public function setUpdated($updated)
+    {
+        $this->updated_at = $updated;
+    }
+
+    public function getUpdated()
+    {
+        return $this->updated_at;
+    }
+
+    public function setCode($code)
+    {
+        $this->code = $code;
+        return $this;
+    }
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
