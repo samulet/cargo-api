c78a0f84879605e511a062cc321a505e42a85577
salerat <salerat@gmail.com>
2013-12-06 22:58:25 +0700

    заново добавил модуль справочников

---
+++ module/Reference/Module.php
@@ -0,0 +1,39 @@
+<?php
+namespace Reference;
+
+use Zend\Db\ResultSet\ResultSet;
+use Reference\Model\ReferenceModel;
+
+class Module
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+
+        return array(
+            'factories' => array(
+                'ReferenceModel' => 'Reference\Factory\ReferenceModelFactory',
+            ),
+        );
+    }
+
+}
