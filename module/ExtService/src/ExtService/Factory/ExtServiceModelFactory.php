451adb1f8c740be10f1aa464f3bedf96b926f787
salerat <salerat@gmail.com>
2013-12-13 01:32:05 +0700

    добавил фабрику для модели

---
+++ module/ExtService/src/ExtService/Factory/ExtServiceModelFactory.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: salerat
+ * Date: 11/15/13
+ * Time: 1:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace ExtService\Factory;
+
+use Zend\Log\Logger;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use ExtService\Model\ExtServiceModel;
+
+class ExtServiceModelFactory implements FactoryInterface {
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $documentManager = $serviceLocator->get('doctrine.documentmanager.odm_default');
+        $queryBuilderModel = $serviceLocator->get('QueryBuilderModel');
+        $config = $serviceLocator->get('config');
+        if(!empty($config)) {
+            $configOnline = $config['online'];
+        } else {
+            $configOnline=null;
+        }
+        return new ExtServiceModel($documentManager,$queryBuilderModel, $configOnline);
+    }
+}
