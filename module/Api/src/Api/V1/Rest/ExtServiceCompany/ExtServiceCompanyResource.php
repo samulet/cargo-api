81fe072badf96248712fdc314a22b467284c6b81
salerat <salerat@gmail.com>
2013-12-17 01:13:04 +0700

    сделал правильное получение данных через курл и подключил модуль

---
+++ module/Api/src/Api/V1/Rest/ExtServiceCompany/ExtServiceCompanyResource.php
@@ -0,0 +1,111 @@
+<?php
+namespace Api\V1\Rest\ExtServiceCompany;
+
+use ZF\ApiProblem\ApiProblem;
+use ZF\Rest\AbstractResourceListener;
+
+class ExtServiceCompanyResource extends AbstractResourceListener {
+
+    protected $extServiceModel;
+    /**
+     * @var \User\Entity\User
+     */
+    protected $userEntity;
+
+    public function __construct($extServiceModel = null,$userEntity=null)
+    {
+        $this->extServiceModel = $extServiceModel;
+        $this->userEntity = $userEntity;
+    }
+    /**
+     * Create a resource
+     *
+     * @param  mixed $data
+     * @return ApiProblem|mixed
+     */
+    public function create($data)
+    {
+        return new ApiProblem(405, 'The POST method has not been defined');
+    }
+
+    /**
+     * Delete a resource
+     *
+     * @param  mixed $id
+     * @return ApiProblem|mixed
+     */
+    public function delete($id)
+    {
+        return new ApiProblem(405, 'The DELETE method has not been defined for individual resources');
+    }
+
+    /**
+     * Delete a collection, or members of a collection
+     *
+     * @param  mixed $data
+     * @return ApiProblem|mixed
+     */
+    public function deleteList($data)
+    {
+        return new ApiProblem(405, 'The DELETE method has not been defined for collections');
+    }
+
+    /**
+     * Fetch a resource
+     *
+     * @param  mixed $id
+     * @return ApiProblem|mixed
+     */
+    public function fetch($id)
+    {
+
+        return new ApiProblem(405, 'The GET method has not been defined for individual resources');
+    }
+
+    /**
+     * Fetch all or a subset of resources
+     *
+     * @param  array $params
+     * @return ApiProblem|mixed
+     */
+    public function fetchAll($params = array())
+    {
+       $this->extServiceModel->getInformationFromOnline(123, 'N6kERS4GrQQh7D42');
+        //return new ApiProblem(405, 'The GET method has not been defined for collections');
+    }
+
+    /**
+     * Patch (partial in-place update) a resource
+     *
+     * @param  mixed $id
+     * @param  mixed $data
+     * @return ApiProblem|mixed
+     */
+    public function patch($id, $data)
+    {
+        return new ApiProblem(405, 'The PATCH method has not been defined for individual resources');
+    }
+
+    /**
+     * Replace a collection or members of a collection
+     *
+     * @param  mixed $data
+     * @return ApiProblem|mixed
+     */
+    public function replaceList($data)
+    {
+        return new ApiProblem(405, 'The PUT method has not been defined for collections');
+    }
+
+    /**
+     * Update a resource
+     *
+     * @param  mixed $id
+     * @param  mixed $data
+     * @return ApiProblem|mixed
+     */
+    public function update($id, $data)
+    {
+        return new ApiProblem(405, 'The PUT method has not been defined for individual resources');
+    }
+}
--- module/ExtService/src/ExtService/Model/ExtServiceModel.php
+++ module/ExtService/src/ExtService/Model/ExtServiceModel.php
@@ -16,6 +16,7 @@ use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 use Doctrine\ODM\MongoDB\Id\UuidGenerator;
 use Zend\Http\Client;
+use Zend\Http\ClientStatic;
 
 class ExtServiceModel {
 
@@ -32,8 +33,50 @@ class ExtServiceModel {
         $this->configOnline=$configOnline;
     }
 
-    public function getInformationFromOnline($code) {
+    public function getInformationFromOnline($url, $code) {
+        /*$client  = new \Zend\Http\Client();
 
+        $client->setUri('http://prodrezerv.altlog.ru/api/reference/companies/');
+        $client->setMethod('GET');
+        $client->setEncType('application/json');
+        $response = $client->send();
+     //   die(var_dump($response));
+        die(var_dump(urldecode($response->getContent()))); */
+
+        $ch = curl_init();
+        $headers = array(
+            'Accept: application/json',
+            'Content-Type: application/json',
+
+        );
+        curl_setopt($ch, CURLOPT_URL, 'http://prodrezerv.altlog.ru/api/reference/companies/');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $body = '{}';
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$body);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // Timeout in seconds
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
+        $authToken = curl_exec($ch);
+        $authToken = json_decode($authToken);
+
+        curl_setopt($ch, CURLOPT_URL, 'http://prodrezerv.altlog.ru/api/reference/companies/?key='.sha1($authToken->token.$code));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $body = '{}';
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$body);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // Timeout in seconds
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        $res = curl_exec($ch);
+        die(var_dump(json_decode($res)));
     }
 
     /**
