2be8510711aa6c687410f887294101468de0b988
salerat <salerat@gmail.com>
2013-12-18 20:13:35 +0700

    добавил недостающие геттеры сеттеры в компанию

--- module/Account/src/Account/Entity/Company.php
+++ module/Account/src/Account/Entity/Company.php
@@ -580,4 +580,52 @@ class Company
     }
 
 
+    /**
+     * @param string $ownerAccUuid
+     */
+    public function setOwnerAccUuid($ownerAccUuid)
+    {
+        $this->ownerAccUuid = $ownerAccUuid;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOwnerAccUuid()
+    {
+        return $this->ownerAccUuid;
+    }
+
+    /**
+     * @param mixed $updated_at
+     */
+    public function setUpdatedAt($updated_at)
+    {
+        $this->updated_at = $updated_at;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @param mixed $created_at
+     */
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
 }
