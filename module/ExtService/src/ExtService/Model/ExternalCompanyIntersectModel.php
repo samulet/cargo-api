3ba088da101995e82022e1d6fdd4383dacb585a1
salerat <salerat@gmail.com>
2013-12-22 23:52:52 +0700

    проведен рефактор переменных, названия класса, часть фукнция вынесела в сервис

---
+++ module/ExtService/src/ExtService/Model/ExternalCompanyIntersectModel.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace ExtService\Model;
+
+use Doctrine\MongoDB\Connection;
+use Doctrine\ODM\MongoDB\Configuration;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
+use Doctrine\ODM\MongoDB\Id\UuidGenerator;
+use Zend\Http\Client;
+use Zend\Http\ClientStatic;
+use ExtService\Entity\ExternalCompany;
+
+class ExternalCompanyIntersectModel
+{
+    protected $documentManager;
+    protected $uuidGenerator;
+    protected $queryBuilderModel;
+    protected $externalCompanyModel;
+
+    public function __construct(DocumentManager $documentManager,$queryBuilderModel,$externalCompanyModel)
+    {
+        $this->uuidGenerator = new UuidGenerator();
+        $this->documentManager=$documentManager;
+        $this->queryBuilderModel=$queryBuilderModel;
+        $this->externalCompanyModel = $externalCompanyModel;
+    }
+
+    public function addCompanyIntersect($data)
+    {
+        $data = array_map('strval', $data);
+        $object = $this->externalCompanyModel->fetch(array('id' => $data['id'], 'source' => $data['source']));
+        if(!empty($object)) {
+            $object->setLink($data['company']);
+            $this->documentManager->persist($object);
+            $this->documentManager->flush();
+        } else {
+            return false;
+        }
+    }
+
+    public function deleteCompanyIntersect($data)
+    {
+        $data = array_map('strval', $data);
+        $object = $this->externalCompanyModel->fetch(array('id' => $data[1], 'source' => $data[0]));
+        if(!empty($object)) {
+            $object->setLink(null);
+            $this->documentManager->persist($object);
+            $this->documentManager->flush();
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @param mixed $externalCompanyModel
+     */
+    public function setExternalCompanyModel($externalCompanyModel)
+    {
+        $this->externalCompanyModel = $externalCompanyModel;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getExternalCompanyModel()
+    {
+        return $this->externalCompanyModel;
+    }
+}
