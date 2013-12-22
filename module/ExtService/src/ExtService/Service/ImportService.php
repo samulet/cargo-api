80d480079c65c2fee6fb7af77f23c86e21d948ad
salerat <salerat@gmail.com>
2013-12-23 00:23:16 +0700

    перенес преобразованеи в камел кейс в квери билдер

--- module/ExtService/src/ExtService/Service/ImportService.php
+++ module/ExtService/src/ExtService/Service/ImportService.php
@@ -32,28 +32,6 @@ class ImportService {
         return json_decode($res);
     }
 
-    /**
-     * Convert under_score type array's keys to camelCase type array's keys
-     * @param   array   $array          array to convert
-     * @param   array   $arrayHolder    parent array holder for recursive array
-     * @return  array   camelCase array
-     */
-    public function camelCaseKeys($array, $arrayHolder = array()) {
-        $camelCaseArray = !empty($arrayHolder) ? $arrayHolder : array();
-        foreach ($array as $key => $val) {
-            $newKey = @explode('_', $key);
-            array_walk($newKey, create_function('&$v', '$v = ucwords($v);'));
-            $newKey = @implode('', $newKey);
-            $newKey{0} = strtolower($newKey{0});
-            if (!is_array($val)) {
-                $camelCaseArray[$newKey] = $val;
-            } else {
-                $camelCaseArray[$newKey] = $this->camelCaseKeys($val, $camelCaseArray[$newKey]);
-            }
-        }
-        return $camelCaseArray;
-    }
-
     public function onlineGetToken($fullUrl)
     {
         $authToken = $this->fetch($fullUrl);
