252f78352ecc20e1e998458adcf9a217cf7fb2f8
salerat <salerat@gmail.com>
2013-12-22 23:51:16 +0700

    фабрики оптимизированы и их названия приведены в соответсвии с названиями моделей

---
+++ module/ExtService/src/ExtService/Factory/ExternalCompanyImportModelFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ExtService\Factory;
+
+use Zend\Log\Logger;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use ExtService\Model\ExternalCompanyImportModel;
+use ExtService\Provider\OnlineProvider;
+
+class ExternalCompanyImportModelFactory implements FactoryInterface
+{
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $config = $serviceLocator->get('config');
+        if(!empty($config)) {
+            $configOnline = $config['online'];
+        } else {
+            $configOnline=null;
+        }
+        return new ExternalCompanyImportModel($serviceLocator->get('doctrine.documentmanager.odm_default'),$serviceLocator->get('QueryBuilderModel'), new OnlineProvider($configOnline),$serviceLocator->get('ExternalCompanyModel'));
+    }
+}
