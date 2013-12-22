a2033c69a760b166113e506c32384176643afb7e
salerat <salerat@gmail.com>
2013-12-22 23:51:16 +0700

    фабрики оптимизированы и их названия приведены в соответсвии с названиями моделей

---
+++ module/ExtService/src/ExtService/Factory/ExternalCompanyIntersectModelFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ExtService\Factory;
+
+use Zend\Log\Logger;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use ExtService\Model\ExternalCompanyIntersectModel;
+
+class ExternalCompanyIntersectModelFactory implements FactoryInterface
+{
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        return new ExternalCompanyIntersectModel($serviceLocator->get('doctrine.documentmanager.odm_default'),$serviceLocator->get('QueryBuilderModel'),$serviceLocator->get('ExternalCompanyModel'));
+    }
+}
