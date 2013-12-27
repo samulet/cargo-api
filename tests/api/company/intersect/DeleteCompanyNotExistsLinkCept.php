8962389b085b3ec1c23db4b703ee811c259a9ebf
Oleg Lobach <oleg@lobach.info>
2013-12-27 16:39:52 +0400

    Добавил к тестам удаления связей проверку записей в БД

--- tests/api/company/intersect/DeleteCompanyNotExistsLinkCept.php
+++ tests/api/company/intersect/DeleteCompanyNotExistsLinkCept.php
@@ -1,6 +1,7 @@
 <?php
 $I = new ApiGuy($scenario);
 $I->wantTo('Try delete not existed link between imported company and internal company');
+$I->seeInCollection('externalCompany', array('source' => 'vesta', 'id' => '28', 'link' => null));
 $I->haveHttpHeader('Content-Type','application/json');
 $I->haveHttpHeader('Accept','*/*');
 $I->haveHttpHeader('X-Auth-UserToken','db057553f1a4989210ae84a2825982e1d04d6879a2690365e1fcecb619fb77e2');
