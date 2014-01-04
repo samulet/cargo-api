f59e434dd363fdae555ac5ae8df033138dde4c44
Oleg Lobach <oleg@lobach.info>
2014-01-04 23:37:02 +0400

    Удалил в конфиге QueryBuilder-а ссылки на не существующий каталог сущностей

--- module/QueryBuilder/config/module.config.php
+++ module/QueryBuilder/config/module.config.php
@@ -8,11 +8,9 @@ return array(
         'driver' => array(
             __NAMESPACE__ . '_driver' => array(
                 'class' => 'Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver',
-                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
             ),
             'odm_default' => array(
                 'drivers' => array(
-                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
                 )
             )
         )
