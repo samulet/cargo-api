e40d7071fff4d876bed86a4af4eecb232361fa2b
Oleg Lobach <oleg@lobach.info>
2014-01-04 14:45:34 +0400

    Исправил тест ресурса, возвращающего список пунктов

--- tests/api/places/GetPlacesListCept.php
+++ tests/api/places/GetPlacesListCept.php
@@ -1,10 +1,36 @@
 <?php
 $I = new ApiGuy($scenario);
 $I->wantTo('Get a list of all companies in system');
+
+$I->haveInCollection('place', [
+    "uuid" => "918a1a12d1784a4cbb2f4360cd1a4d07",
+    "name" => "Веневская (Грин Трейд)",
+    "company" => [
+        "id" => new MongoId("52b2480f9e53462e6e8b456e"),
+        "name" => "demo56",
+        "uuid" => "c14bfc17646343b4afc037fb3c8c5391",
+    ],
+    "purpose" => [ 'ul' ],
+    "address" => [
+        "country" => "Российская Федерация",
+        "city" => "Москва",
+        "street" => "Веневская, ул",
+        "house" => "6"
+    ],
+    "created" => new MongoDate(1388831431000),
+    "creator" => [
+        "id" => new MongoId("52a4b7519e534607468b456f"),
+        "name" => "Some User",
+        "uuid" => "93456a97789c4538ba8d0e8d7419e658"
+    ]
+]);
+
+
 $I->haveHttpHeader('Content-Type', 'application/json');
 $I->haveHttpHeader('Accept', '*/*');
 $I->haveHttpHeader('X-Auth-UserToken', 'db057553f1a4989210ae84a2825982e1d04d6879a2690365e1fcecb619fb77e2');
 $I->sendGET('places');
+
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson(array(
@@ -12,4 +38,4 @@ $I->seeResponseContainsJson(array(
         "places" => array(),
     ),
 ));
-$I->SeeResponseContains('"uuid":"afc66c7dd6234e568317e4799068a03b"');
+$I->SeeResponseContains('"uuid":"918a1a12d1784a4cbb2f4360cd1a4d07"');
